116cf96b2ce754800277ab3f6638befab956fc5c
e-lie <mail@eliegavoty.fr>
2019-08-13 02:07:14 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+
+extra_pkg_dependencies="php7.3-zip php7.3-gd php7.3-xml php7.3-mbstring"
+
+
 # Execute a command as another user
 # usage: exec_as USER COMMAND [ARG ...]
 exec_as() {
